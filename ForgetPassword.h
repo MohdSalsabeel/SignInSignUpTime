2dd9e76cfd62a087ae409c0b6b7ed9ff12dcf93b
MohdSalsabeel <103497056+MohdSalsabeel@users.noreply.github.com>
2022-05-06 17:36:12 +0530

    Create ForgetPassword.h

---
+++ ForgetPassword.h
@@ -0,0 +1,16 @@
+#ifndef __ForgetPassword_h
+#define __ForgetPassword_h
+
+#include<string>
+#include "User_module.h"
+
+
+class ForgetPassword : public User 
+{
+    public:
+        string pins;
+        string times;
+        void forget();
+};
+
+#endif
